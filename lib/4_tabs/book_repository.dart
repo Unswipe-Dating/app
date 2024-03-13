ddca0401ce1f6aaa86473b34174050a4cd20c8e8
Abhishek Tiwari <abhitiwari472@gmail.com>
2024-03-14 03:23:23 +0530

    added core files

--- lib/4_tabs/book_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'models/book.dart';
-
-final bookRepository = [
-  Book(0, 'Stranger in a Strange Land', 'Robert A. Heinlein'),
-  Book(1, 'Foundation', 'Isaac Asimov'),
-  Book(2, 'Fahrenheit 451', 'Ray Bradbury'),
-];
--- lib/4_tabs/router/tab_enum.dart
+++
@@ -1,4 +0,0 @@
-enum TabEnum {
-  books,
-  about,
-}
