2914331bbc7fffdf155b61f1c39391c3e4530878
Abhishek Tiwari <abhitiwari472@gmail.com>
2024-03-20 19:13:37 +0530

    updated splash page

--- lib/src/features/splash/presentation/pages/splash_page.dart
+++ lib/src/features/splash/presentation/pages/splash_page.dart
@@ -10,6 +10,21 @@ import '../../../../core/helper/helper.dart';
 import '../../../../core/utils/injections.dart';
 
 
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const SplashPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
