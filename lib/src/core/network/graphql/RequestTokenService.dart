3c3346e98f5e340d54426d6fd6f11992b64b13c3
Abhishek Tiwari <abhitiwari472@gmail.com>
2024-07-14 23:49:38 +0530

    .

--- lib/src/core/network/graphql/RequestTokenService.dart
+++ lib/src/core/network/graphql/RequestTokenService.dart
@@ -1,41 +1,45 @@
 import 'dart:developer';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:injectable/injectable.dart';
 import 'package:unswipe/src/features/login/data/models/request_otp/otp_response.dart';
+import 'package:unswipe/src/features/login/data/models/signupOrLogin/signup_response.dart';
 import 'package:unswipe/src/features/login/data/models/verify_otp/verify_otp_response.dart';
 import 'package:unswipe/src/features/login/domain/repository/login_repository.dart';
-import 'package:unswipe/src/features/splash/data/datasources/model/response_meta.dart';
-import 'package:unswipe/src/features/userOnboarding/contact_block/data/model/response_contact_block.dart';
-import 'package:unswipe/src/features/userProfile/data/model/create_request/response_profile_request.dart';
 import '../../../../../../../data/api_response.dart';
-import '../../../../../core/network/graphql/graphql_service.dart';
+import 'graphql_service.dart';
 
 @injectable
-class MetaService {
-  MetaService(this.service);
+class RequestTokenService {
+  RequestTokenService(this.service);
 
   final GraphQLService service;
 
-  Future<ApiResponse<ResponseMeta>> getMeta(String token
+  Future<ApiResponse<SignUpResponse>> refreshToken(
+      String token,
+      String userId
       ) async {
-    final query = '''
-    query GetConfig {
-    getConfig 
+    const query = '''
+   mutation refreshToken(\$token: String!,\$userId: String!) {
+  refreshToken(data: {
+    token: \$token,
+    userId: \$userId
+  }) {
+    accessToken
+    refreshToken
+  }
 }
-''';
-
-
-
+    ''';
 
     try {
-      final response = await service.performMutationWithHeader(
-          token, query, variables: {});
+      final response = await service.performMutation(query, variables: {
+        "token": token,
+        "userId": userId,
+      });
       log('$response');
 
       if (!response.hasException) {
-        ResponseMeta? info;
+        SignUpResponse? info;
         try {
-          info = ResponseMeta.fromJson(
+          info = SignUpResponse.fromJson(
             response.data as Map<String, dynamic>,
           );
         } on Exception catch (e) {
@@ -46,7 +50,6 @@ class MetaService {
       } else {
         if (response.exception?.graphqlErrors[0].extensions?['code'] ==
             "UNAUTHENTICATED") {
-
           return AuthorizationFailure(error: response.exception);
         }
         return OperationFailure(error: response.exception);
@@ -54,7 +57,6 @@ class MetaService {
     } on TimeOutFailure catch (_) {
       // todo: timeout failure
       return TimeOutFailure();
-
     }
   }
 
--- lib/src/features/login/data/datasources/network/service/otp_service.dart
+++ lib/src/features/login/data/datasources/network/service/otp_service.dart
@@ -166,50 +166,5 @@ class OtpService {
     }
   }
 
-  Future<ApiResponse<SignUpResponse>> refreshToken(
-      String token,
-      String userId
-      ) async {
-    const query = '''
-   mutation refreshToken(\$token: String!,\$userId: String!) {
-  refreshToken(data: {
-    token: \$token,
-    userId: \$userId
-  }) {
-    accessToken
-    refreshToken
-  }
-}
-    ''';
 
-    try {
-      final response = await service.performMutation(query, variables: {
-        "token": token,
-        "userId": userId,
-      });
-      log('$response');
-
-      if (!response.hasException) {
-        SignUpResponse? info;
-        try {
-          info = SignUpResponse.fromJson(
-            response.data as Map<String, dynamic>,
-          );
-        } on Exception catch (e) {
-          log('error', error: e);
-          return Failure(error: Exception(e));
-        }
-        return Success(data: info);
-      } else {
-        if (response.exception?.graphqlErrors[0].extensions?['code'] ==
-            "UNAUTHENTICATED") {
-          return AuthorizationFailure(error: response.exception);
-        }
-        return OperationFailure(error: response.exception);
-      }
-    } on TimeOutFailure catch (_) {
-      // todo: timeout failure
-      return TimeOutFailure();
-    }
-  }
 }
