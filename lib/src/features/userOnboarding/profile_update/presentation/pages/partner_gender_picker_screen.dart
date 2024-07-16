2aebe753aa87e2dc8bfb815986c883f3786aee1c
Abhishek Tiwari <abhitiwari472@gmail.com>
2024-07-17 03:40:36 +0530

    added partner gender picker screen changes

--- lib/src/features/userOnboarding/profile_update/presentation/pages/partner_gender_picker_screen.dart
+++ lib/src/features/userOnboarding/profile_update/presentation/pages/partner_gender_picker_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:unswipe/src/features/login/presentation/pages/Login.dart';
 import 'package:unswipe/src/features/settings/domain/repository/user_settings_repository.dart';
 
 import '../../../../../core/router/app_router.dart';
@@ -71,8 +72,8 @@ class _PartnerGenderUpdateScreenState extends State<PartnerGenderUpdateScreen> {
                     ),
                   ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
                   child: Text(
                     textAlign: TextAlign.start,
                     'You prefer to date',
@@ -83,8 +84,8 @@ class _PartnerGenderUpdateScreenState extends State<PartnerGenderUpdateScreen> {
                         fontSize: 24.0),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
                   child: Text(
                     "This will be shown on your profile. You can change this later.",
                     style: TextStyle(
@@ -179,7 +180,7 @@ class _PartnerGenderUpdateScreenState extends State<PartnerGenderUpdateScreen> {
                 Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "Show only first letter",
+                      "Display on profile",
                       style: TextStyle(
                           color: Colors.grey[900],
                           fontFamily: 'lato',
@@ -202,8 +203,8 @@ class _PartnerGenderUpdateScreenState extends State<PartnerGenderUpdateScreen> {
 
                 Padding(
                   padding: EdgeInsets.all(16),
-                  child: ElevatedButton(
-                    onPressed: isButtonEnabled ? () {
+                  child: CustomButton(
+                    onPressed: () {
                       if(widget.params.profileParams != null) {
                         Navigator.pop(mContext, getNameFromCharacter(_character));
 
@@ -217,27 +218,9 @@ class _PartnerGenderUpdateScreenState extends State<PartnerGenderUpdateScreen> {
                                 .getUpdatedParams(widget.params.updateParams)
                         );
                       }
-                    } : null,
-                    style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.black,
-                        disabledBackgroundColor: Colors.black.withOpacity(0.6),
-                        disabledForegroundColor: Colors.white.withOpacity(0.6),
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                          BorderRadius.circular(2.0), // Rounded corners
-                        ),
-                        minimumSize:
-                        const Size.fromHeight(48) // Set button text color
-                    ),
-                    child: const Text(
-                      'Upload',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Lato',
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18.0),
-                    ),
+                    },
+                    text: 'Next',
+                    isEnabled: isButtonEnabled,
                   ),
                 ),
               ],
