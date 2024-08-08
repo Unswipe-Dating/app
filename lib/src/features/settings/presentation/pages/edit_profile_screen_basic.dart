da45763b6bc2dd4667e8cfc829413e62a97b88d9
Abhishek Tiwari <abhitiwari472@gmail.com>
2024-08-08 11:20:54 +0530

    .

--- lib/src/features/settings/presentation/pages/edit_profile_screen_basic.dart
+++ lib/src/features/settings/presentation/pages/edit_profile_screen_basic.dart
@@ -47,16 +47,16 @@ class EditProfileScreenBasic extends StatefulWidget {
 
 class _EditProfileScreenBasicState extends State<EditProfileScreenBasic> {
   String data = "";
-  ProofRequest proofRequest = ProofRequest(applicationId: '0x1DD4a325bD51B09C7f840D54de854266ECB4697A');
+  ProofRequest proofRequest = ProofRequest(applicationId: '');
   void startVerificationFlow() async {
     final providerIds = [
-      '85de83e5-5635-4768-9f76-f9f04f7661a1', // Aadhar General Info
+      '', // Aadhar General Info
     ];
     proofRequest.setAppCallbackUrl(CustomNavigationHelper.settingsPathBasic);
     await proofRequest
         .buildProofRequest(providerIds[0]);
     proofRequest.setSignature(proofRequest.generateSignature(
-        '0xf232d1bac6350a49fd5d86015c8a1191b2e724729560fb4a18638fa9f88436d2'));
+        ''));
     final verificationRequest = await proofRequest.createVerificationRequest();
     final startSessionParam = StartSessionParams(
       onSuccessCallback: (proof) => setState(() {
